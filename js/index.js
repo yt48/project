bb08a638d54d49bdb8266e299770b45c6044bed1
Yaroslav Tymoschuk <roger008889@gmail.com>
2017-10-05 21:34:31 +0000

    Comment

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// /*global $*/
+// $(function() {
+//     $('.smooth').on('click',function(event) {
+//         var target = $(this.getAttribute('href'));
+//         if (target.length) {
+//             event.preventDefault();
+//             $('html,body').stop().animate({
+//                 scrollTop: target.offset().top
+//             },1000);
+//         }
+//     });
+// });
